5a3c8556b76b6007c416fca2e530d2626a4e3bde
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-03-15 19:51:42 +0530

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace View_Bag.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Sending Simple Data
+            base.ViewBag.FirstName = "Vinit";
+            base.ViewBag.LastName = "Suryarao";
+
+            // Sending COmplex Data By Convert in Json
+            var clientModelObj = new
+            {
+                FirstName = "Avinash",
+                LastName = "Suryarao"
+            };
+
+            var clientModelJson = JsonConvert.SerializeObject(clientModelObj);
+            base.ViewBag.ClientModel = clientModelJson;
+
+            return View();
+        }
+    }
+}
